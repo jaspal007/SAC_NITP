7acec2190ee3c5e22395d87c8807cc52729a3b3b
JASPAL <jaspalsinghgkp512@gmail.com>
2023-04-08 04:36:20 +0530

    data update

--- lib/utility/global_variable.dart
+++ lib/utility/global_variable.dart
@@ -13,18 +13,733 @@ final games = <String, String>{
   'BADMINTON': 'lib/assets/badminton.jpg',
 };
 
-final coord = <String, String>{
-  'TABLE TENNIS': 'Gautum Rishi',
-  'CHESS': 'Kaushik Pattinaik',
-  'ATHLETICS': 'Vamsi Pendliv',
-  'BASKETBALL': 'Avinash Prem',
-  'FOOTBALL': 'Sameer Nautial',
-  'VOLLEYBALL': 'MD Nawab Azad',
-  'CRICKET': 'Ankit Mahto',
-  'POWER SPORTS': 'Vijay Sharma',
-  'KABADDI(W)': 'Priya Singh',
-  'KABADDI(M)': 'Rajaram',
-  'BADMINTON': 'Vishal Kumar',
+final coord = {
+  'TABLE TENNIS': [
+    {
+      'Coordinator': [
+        {'Name': 'GAUTAM RISHI'},
+        {'RollNum': '1905036'},
+        {'Mob': '6207828411'},
+        {'email': 'gautamr.ug19.ar@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': 'ADITYA KUMAR SINGH'},
+        {'RollNum': '215CE050'},
+        {'Mob': '9304278609'},
+        {'email': 'adityass.ph21.ce@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': 'ANWAR HUSSAIN'},
+        {'RollNum': '1810008'},
+        {'Mob': '7006652224'},
+        {'email': 'anwarh.ug18.ph@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': 'SAI ROHIT ARROJU'},
+        {'RollNum': '2004092'},
+        {'Mob': '9885029600'},
+        {'email': 'saia.ug20.ece@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': 'VISHESH VERMA'},
+        {'RollNum': '2104118'},
+        {'Mob': '8840254948'},
+        {'email': 'visheshv.ug21.ec@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': 'PRATIK RAJ'},
+        {'RollNum': '2201044'},
+        {'Mob': '7667898311'},
+        {'email': 'pratikr.ug22.me@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': 'PRIYASH KETKAR'},
+        {'RollNum': '2205036'},
+        {'Mob': '7428323874'},
+        {'email': 'priyashk.ug22.ar@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': 'ADITYA KUMAR KASHYAP'},
+        {'RollNum': '2006023'},
+        {'Mob': '7091049043'},
+        {'email': 'adityak.ug20.cse@nitp.ac.in'}
+      ]
+    },
+  ],
+  'CHESS': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'ATHLETICS': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'BASKETBALL': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'FOOTBALL': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'VOLLEYBALL': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'CRICKET': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'POWER SPORTS': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'KABADDI(W)': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'KABADDI(M)': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
+  'BADMINTON': [
+    {
+      'Coordinator': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Co_ordinator2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member1': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member2': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member3': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member4': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+    {
+      'Member5': [
+        {'Name': ''},
+        {'RollNum': ''},
+        {'Mob': ''},
+        {'email': '@nitp.ac.in'}
+      ]
+    },
+  ],
 };
 
 List<String> teams = <String>[
@@ -35,8 +750,155 @@ List<String> teams = <String>[
   'Women',
   'Mech',
   'EE',
-  'ECE'
+  'ECE',
 ];
+
+final teamCord = {
+  'CSE': [
+    {
+      'Coordinator': [
+        {'name': 'AADITYA RAJ'},
+        {'RollNo': '2006165'},
+        {'Mob': '7742538482'},
+        {'Email': 'aadityar.ug20.cse@nitp.ac.in'},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': 'RAJNEESH YADAV '},
+        {'RollNo': '2006049'},
+        {'Mob': '9057521568'},
+        {'Email': 'yadavd.ug20.cse@nitp.ac.in'},
+      ],
+    },
+  ],
+  'Arch': [
+    {
+      'Coordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+  ],
+  'Int. MSc': [
+    {
+      'Coordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+  ],
+  'Civil': [
+    {
+      'Coordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+  ],
+  'Women': [
+    {
+      'Coordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+  ],
+  'Mech': [
+    {
+      'Coordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+  ],
+  'EE': [
+    {
+      'Coordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+  ],
+  'ECE': [
+    {
+      'Coordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+    {
+      'Co_ordinator': [
+        {'name': ''},
+        {'RollNo': ''},
+        {'Mob': ''},
+        {'Email': ''},
+      ],
+    },
+  ],
+};
 
 class GlobalVariable {
   final sports = games.keys.toList(growable: false);
@@ -44,7 +906,8 @@ class GlobalVariable {
   Map<String, String> getImages() => games;
   final lead = coord.keys.toList(growable: false);
   getCoord() => lead;
-  Map<String, String> getCoordNames() => coord;
+  Map<String, List<Map<String, List<Map<String, String>>>>> getCoordNames() =>
+      coord;
   //void setGames(List<String>? value) => games.keys.toList() = value!;
   List<String> getTeams() => teams;
   void setTeams(List<String>? value) => teams = value!;
